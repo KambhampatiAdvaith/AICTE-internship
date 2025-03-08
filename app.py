7af0d59fef6e7968ba68703d18b639a6520d71f9
K Advaith <162140222+KambhampatiAdvaith@users.noreply.github.com>
2025-03-08 14:33:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,6 @@ st.markdown(page_bg_img, unsafe_allow_html=True)
 
 # Load the saved models
 models = {
-    'diabetes': pickle.load(open('Models/diabetes_model.sav', 'rb')),
     'heart_disease': pickle.load(open('Models/heart_disease_model.sav', 'rb')),
     'parkinsons': pickle.load(open('Models/parkinsons_model.sav', 'rb')),
     'lung_cancer': pickle.load(open('Models/lungs_disease_model.sav', 'rb')),
